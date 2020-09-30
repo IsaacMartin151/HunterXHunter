e7ac30478a7b41da023559e6f6e39055e8a4defe
Isaac Martin <Martiisa@oregonstate.edu>
2020-09-30 11:46:52 -0700

    Starting Mod, couple of experimental items/blocks

---
+++ HunterXHunter/src/main/java/com/chubbychump/hunterxhunter/HunterXHunter.java
@@ -0,0 +1,33 @@
+package com.chubbychump.hunterxhunter;
+
+
+import com.chubbychump.hunterxhunter.util.RegistryHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("hunterxhunter")
+public class HunterXHunter
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "hunterxhunter";
+
+
+    public HunterXHunter() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        // Register ourselves for server and other game events we are interested in
+        RegistryHandler.init();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void setup(final FMLCommonSetupEvent event) { }
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+}
