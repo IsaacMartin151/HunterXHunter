7ca9f4699d18168cf605fabadaa0371bc0fab5d9
Isaac Martin <IsaacMartin151@gmail.com>
2022-10-22 22:07:17 -0700

    Refactoring the base repo, removing unnecessary files

---
+++ src/main/java/com/registry/Blocks.java
@@ -0,0 +1,16 @@
+package com.registry;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static com.example.hunterxhunter.HunterXHunter.MODID;
+
+public class Blocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
+}
